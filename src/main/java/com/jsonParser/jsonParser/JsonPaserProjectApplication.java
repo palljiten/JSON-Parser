2e1e8437a8d2dfc3e518dd532609e5d720a5dc9d
jitpal <palljiten@gmail.com>
2025-09-16 17:15:57 +0530

    Json Parser Initialization

---
+++ src/main/java/com/jsonParser/jsonParser/JsonPaserProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jsonParser.jsonParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPaserProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonPaserProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsonParser/jsonParser/JsonPaserProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsonParser.jsonParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPaserProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
